b437e1c9bf4332de270b72e546cc19cc5064c885
jalahashimli <jlaah2003@gmail.com>
2025-07-11 21:16:17 +0400

    lesson 139 is finished

--- 09-Omnifood Optimizations/js/script.js
+++ 09-Omnifood Optimizations/js/script.js
@@ -5,10 +5,26 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-h1.addEventListener("click", function () {
-  h1.textContent = myName;
-  h1.style.backgroundColor = "red";
-  h1.style.padding = "5rem";
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+///////////////////////////////////////////////////////////
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
 ///////////////////////////////////////////////////////////
