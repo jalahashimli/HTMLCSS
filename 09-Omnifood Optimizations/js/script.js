bb5f6c31441c7c9b7f2260cfe480c5b205ffd764
jalahashimli <jlaah2003@gmail.com>
2025-07-12 19:19:44 +0400

    lesson 140 is finished

--- 09-Omnifood Optimizations/js/script.js
+++ 09-Omnifood Optimizations/js/script.js
@@ -13,11 +13,12 @@ console.log(h1);
 
 ///////////////////////////////////////////////////////////
 // Set current year
+
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
-///////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
@@ -28,7 +29,37 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
